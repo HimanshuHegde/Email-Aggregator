b3af7426642459bbf8cc2895912743907562feb6
HimanshuHegde <himanshuhegde797@gmail.com>
2025-11-23 12:05:21 +0530

    chore: update dependencies and modify prisma configuration
    - Updated @prisma/client and prisma to version 7.0.0 in package.json.
    - Added typescript as a devDependency.
    - Changed import statement for dotenv in prisma.config.ts to use ES module syntax.
    - Commented out the engine property in prisma.config.ts.

--- Backend/prisma.config.ts
+++ Backend/prisma.config.ts
@@ -1,11 +1,11 @@
-// import "dotenv/config";
+import "dotenv/config";
 import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
-  engine: "classic",
+  // engine: "classic",
   datasource: {
     url: process.env.DATABASE_URL! ,
   },
